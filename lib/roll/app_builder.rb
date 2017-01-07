769c1b9d209f6c94b53d8aaf57f0e74d1dbbd7d2
Mehdi Lahmam <mehdi@lahmam.com>
2017-01-07 20:42:11 +0100

    Set development delivery_method to :file
    `:test` sends the emails to the logs, and to see them one has to scroll
    back through them. If we configure delivery_method to be `:file`,
    mails will also be created in the `./tmp/mails/` directory
    
    One file will be created for each email address sent to that contains
    all email content delivered to that address. This allows to easily
    review any and all emails sent, without searching through logs.

--- lib/roll/app_builder.rb
+++ lib/roll/app_builder.rb
@@ -63,7 +63,7 @@ module Roll
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :file
       RUBY
 
       inject_into_file(
